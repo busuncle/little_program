b04945915edf521c1995efdc16e8c94dfb81752d
busuncle <lizhij2@gmail.com>
2013-03-16 11:30:08 +0800

    improve, comment
    improve last step in viterbi: find the optimal path with backward
    pointer, add more comment, remove useless import

--- python/viterbi.py
+++ python/viterbi.py
@@ -4,8 +4,6 @@
 a demo for learning Hidden Markov Model(HMM) and Viterbi algorithm
 """
 
-import random
-
 
 
 states = ("s1", "s2")
@@ -22,34 +20,48 @@ emission_matrix = {
 
 
 def viterbi(obs, stat, start_p, trans_m, emit_m):
-    # the two to-be-fill matrix, one contains probability, the other contains path node
-    optimal_prob_m = [{}]
-    optimal_path_m = [{}]
+    """
+    the two to-be-fill matrix, one contains optimal probability, 
+    the other contains optimal state(a backward pointer for constructing the viterbi path)
+
+            S1      S2      ...     Sn
+    O(t=0)  P01     P02     ...     P0n
+    O(t=1)  P11     P12     ...     P1n
+                            ...
+    O(t=m)  Pm1     Pm2     ...     Pmn
+
+            S1      S2      ...     Sn
+    O(t=0)  S01     S02     ...     S0n
+    O(t=1)  S11     S12     ...     S1n
+                            ...
+    O(t=m)  Sm1     Sm2     ...     Smn
+    """
+    prob_matrix = [{}]
+    stat_matrix = [{}]
 
     # init start status
     for s in stat:
-        optimal_prob_m[0][s] = start_p[s] * emit_m[s][obs[0]]
-        optimal_path_m[0][s] = s
+        prob_matrix[0][s] = start_p[s] * emit_m[s][obs[0]]
+        stat_matrix[0][s] = s
 
     # using dynamic programming method to fill the two matrix in time sequence t1, t2, ..., tn
     for t in xrange(1, len(obs)):
-        optimal_prob_m.append({})
-        optimal_path_m.append({})
+        prob_matrix.append({})
+        stat_matrix.append({})
 
         for s in stat:
-            optimal_prob, optimal_stat = max([(optimal_prob_m[t - 1][si] * trans_m[si][s] * emit_m[s][obs[t]], si) for si in stat])
+            optimal_prob, optimal_stat = max(
+                [(prob_matrix[t - 1][si] * trans_m[si][s] * emit_m[s][obs[t]], si) for si in stat])
             # optimal probability for state s in time t
-            optimal_prob_m[t][s] = optimal_prob
+            prob_matrix[t][s] = optimal_prob
             # backward pointer for optimal_stat
-            optimal_path_m[t][s] = optimal_stat
+            stat_matrix[t][s] = optimal_stat
 
-    # output a path containing optimal sequence
-    path = [max(optimal_prob_m[-1].iteritems(), key=lambda x:x[1])[0]]
+    # find the optimal path with backward pointer, state at t=0 is an initial value and will be ignored
+    path = [max(prob_matrix[-1].iteritems(), key=lambda x:x[1])[0]]
     for i in xrange(len(obs) - 1, 0, -1):
-        for s, ps in optimal_path_m[i].iteritems():
-            if s == path[-1]:
-                path.append(ps)
-                break
+        pre_state = stat_matrix[i][path[-1]]
+        path.append(pre_state)
 
     return path[::-1]
 
