d09bca57fd98fdf847226002d9fe6d87cb698aa9
busuncle <lizhij2@gmail.com>
2013-08-05 22:46:05 +0800

    add connect_by_2

--- lua/lianliankan.lua
+++ lua/lianliankan.lua
@@ -14,6 +14,12 @@ local test_maps = {
         {1, nil, 1, 2},
         {2, 3, nil, 3},
     },
+    {
+        {1, nil, 2},
+        {4, 1, nil},
+        {3, 3, nil},
+        {4, 2, nil},
+    },
 }
 
 local function connect_by_1(p1, p2, map)
@@ -38,10 +44,32 @@ local function connect_by_1(p1, p2, map)
     return false
 end 
 
+local function connect_by_2(p1, p2, map)
+    -- try 2 dimensions from p1 to p2
+    local step = (p2.row - p1.row) / math.abs(p2.row - p1.row)
+    for i = p1.row+step, p2.row, step do
+        if connect_by_1({row=i,column=p1.column}, p2, map) then
+            return true
+        end
+    end
+
+    step = (p2.column - p1.column) / math.abs(p2.column - p1.column)
+    for i = p1.column+step, p2.column, step do
+        if connect_by_1({row=p1.row,column=i}, p2, map) then
+            return true
+        end
+    end
+    return false
+end
+
 print("---------begin-------------")
-print(connect_by_1({row=1,column=2}, {row=2,column=1}, test_maps[1])) --false
-print(connect_by_1({row=1,column=3}, {row=2,column=3}, test_maps[1])) --true
-print(connect_by_1({row=3,column=1}, {row=3,column=4}, test_maps[1])) --false
-print(connect_by_1({row=1,column=2}, {row=2,column=1}, test_maps[2])) --false
-print(connect_by_1({row=1,column=1}, {row=1,column=3}, test_maps[3])) --true
+print(connect_by_1({row=1,column=2}, {row=2,column=1}, test_maps[1]) == true) --false
+print(connect_by_1({row=1,column=3}, {row=2,column=3}, test_maps[1]) == true) --true
+print(connect_by_1({row=3,column=1}, {row=3,column=4}, test_maps[1]) == false) --false
+print(connect_by_1({row=1,column=2}, {row=2,column=1}, test_maps[2]) == false) --false
+print(connect_by_1({row=1,column=1}, {row=1,column=3}, test_maps[3]) == true) --true
+
+print(connect_by_2({row=1,column=1}, {row=2,column=2}, test_maps[4]) == true) --true
+print(connect_by_2({row=1,column=3}, {row=4,column=2}, test_maps[4]) == true) --true
+print(connect_by_2({row=2,column=1}, {row=4,column=1}, test_maps[4]) == false) --false
 print("---------end-------------")
