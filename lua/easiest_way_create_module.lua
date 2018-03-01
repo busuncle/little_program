89e44120f10c93ab79b00ded08937ffbca55ddeb
busuncle <lizhij2@gmail.com>
2018-03-01 23:34:27 +0800

    modify name

--- lua/easiest_way_create_module.lua
+++ lua/easiest_way_create_module.lua
@@ -1,13 +1,13 @@
 -- first line, create this table, name it what you want, _M is good
-local this_is_a_module = {}
+local _M = {}
 
-this_is_a_module.sub_module_a = {}
-function this_is_a_module.function_a()
+_M.sub_module_a = {}
+function _M.function_a()
     return "this is a function of the module"
 end
 
 -- the last line, return this table
-return this_is_a_module
+return _M
 
 
 --[[
